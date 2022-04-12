67534dd72f8cc8c1822e62e0989d79718f24cf5f
pythonDev161221 <ormonkenenbaev@gmail.com>
2022-04-12 11:23:43 +0600

    Добавлены модели TypeOfCleaning и TypeOfObject, и промежуточная модель между ними TypeOfObjectAndClean

--- source/crmapp/admin.py
+++ source/crmapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from crmapp.models import TypeOfCleaning, TypeOfObjectAndClean, TypeOfObject
+
+admin.site.register(TypeOfCleaning)
+admin.site.register(TypeOfObjectAndClean)
+admin.site.register(TypeOfObject)
--- source/crmapp/models.py
+++ source/crmapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+
+class TypeOfCleaning(models.Model):
+    cleaning_type = models.CharField(max_length=200, null=False, blank=False)
+
+    def __str__(self):
+        return f"{self.cleaning_type}"
+
+
+class TypeOfObjectAndClean(models.Model):
+    object_to_clean = models.ForeignKey('crmapp.TypeOfObject', on_delete=models.CASCADE)
+    cleaning = models.ForeignKey(TypeOfCleaning, on_delete=models.CASCADE)
+    price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.cleaning} {self.object_to_clean} {self.price}"
+
+
+class TypeOfObject(models.Model):
+    object_type = models.CharField(max_length=200, null=False, blank=False)
+    cleanings = models.ManyToManyField(TypeOfCleaning, through=TypeOfObjectAndClean,
+                                       related_name='type_of_objects')
+
+    def __str__(self):
+        return f"{self.object_type}"
