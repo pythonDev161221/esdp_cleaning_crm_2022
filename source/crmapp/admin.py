091ae3b3580ef3e6d55dba0d7616cfc3a69c0706
nargiza-jumagulova <impression1108@gmail.com>
2022-04-13 14:38:55 +0600

    #4 Создана модель ExtraService для дополнительной услуги, модель зарегистрирована в админке, созданы фикстуры

--- source/crmapp/admin.py
+++ source/crmapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from crmapp.models import ExtraService
+
+
+class ExtraServiceAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_display_links = ['id', 'name']
+    fields = ['name', 'unit', 'price', 'cleaning_time']
+
+
+admin.site.register(ExtraService, ExtraServiceAdmin)
--- source/crmapp/models.py
+++ source/crmapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ExtraService(models.Model):
+    name = models.CharField(max_length=300, null=False, blank=False, verbose_name="Дополнительная услуга")
+    unit = models.CharField(max_length=350, null=True, blank=True, verbose_name="Единица измерения")
+    price = models.PositiveIntegerField(verbose_name="Цена", null=False, blank=False)
+    cleaning_time = models.CharField(max_length=255, verbose_name="Время уборки", null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        db_table = 'extra_services'
+        verbose_name = 'extra_service'
+        verbose_name_plural = 'extra_services'
