420aa1b5c7ed457a17a1942d4c007facff82c28f
pythonDev161221 <ormonkenenbaev@gmail.com>
2022-04-21 03:43:09 +0600

    #18 тикет. Переупаковал файлы в service.

--- source/crmapp/views/service_views.py
+++ source/crmapp/views/service_views.py
@@ -7,21 +7,21 @@ from crmapp.models import Service, PropertySort, CleaningSort
 
 class ServiceListView(ListView):
     model = Service
-    template_name = 'service/service_list.html'
+    template_name = 'service/service/service_list.html'
     context_object_name = 'services'
 
 
 class ServiceCreateView(CreateView):
     model = Service
     form_class = ServiceForm
-    template_name = 'service/service_create.html'
+    template_name = 'service/service/service_create.html'
     success_url = reverse_lazy('crmapp:service_list')
 
 
 class ServiceUpdateView(UpdateView):
     model = Service
     form_class = ServiceForm
-    template_name = 'service/service_update.html'
+    template_name = 'service/service/service_update.html'
 
     def get_success_url(self):
         return reverse("crmapp:service_list")
@@ -29,33 +29,33 @@ class ServiceUpdateView(UpdateView):
 
 class ServiceDeleteView(DeleteView):
     model = Service
-    template_name = 'service/service_delete.html'
+    template_name = 'service/service/service_delete.html'
     success_url = reverse_lazy('crmapp:service_list')
 
 
 class PropertySortListView(ListView):
     model = PropertySort
     context_object_name = 'property_sorts'
-    template_name = 'service/property_sort_list.html'
+    template_name = 'service/property_sort/property_sort_list.html'
 
 
 class PropertySortCreateView(CreateView):
     model = PropertySort
     form_class = PropertySortForm
     success_url = reverse_lazy('crmapp:property_sort_list')
-    template_name = 'service/property_sort_create.html'
+    template_name = 'service/property_sort/property_sort_create.html'
 
 
 class PropertySortUpdateView(UpdateView):
     model = PropertySort
     form_class = PropertySortForm
-    template_name = 'service/property_sort_update.html'
+    template_name = 'service/property_sort/property_sort_update.html'
     success_url = reverse_lazy('crmapp:property_sort_list')
 
 
 class PropertySortDeleteView(DeleteView):
     model = PropertySort
-    template_name = 'service/property_sort_delete.html'
+    template_name = 'service/property_sort/property_sort_delete.html'
     context_object_name = 'property_sort'
     success_url = reverse_lazy('crmapp:property_sort_list')
 
@@ -63,25 +63,25 @@ class PropertySortDeleteView(DeleteView):
 class CleaningSortListView(ListView):
     model = CleaningSort
     context_object_name = 'cleaning_sorts'
-    template_name = 'service/cleaning_sort_list.html'
+    template_name = 'service/cleaning_sort/cleaning_sort_list.html'
 
 
 class CleaningSortCreateView(CreateView):
     model = CleaningSort
     form_class = CleaningSortForm
     success_url = reverse_lazy('crmapp:cleaning_sort_list')
-    template_name = 'service/cleaning_sort_create.html'
+    template_name = 'service/cleaning_sort/cleaning_sort_create.html'
 
 
 class CleaningSortUpdateView(UpdateView):
     model = CleaningSort
     form_class = CleaningSortForm
     success_url = reverse_lazy('crmapp:cleaning_sort_list')
-    template_name = 'service/cleaning_sort_update.html'
+    template_name = 'service/cleaning_sort/cleaning_sort_update.html'
 
 
 class CleaningSortDeleteView(DeleteView):
     model = CleaningSort
     success_url = reverse_lazy('crmapp:cleaning_sort_list')
     context_object_name = 'cleaning_sort'
-    template_name = 'service/cleaning_sort_delete.html'
+    template_name = 'service/cleaning_sort/cleaning_sort_delete.html'
