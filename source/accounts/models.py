af7effefc447e4806686c303c0ab10ec1b1f9ec3
Aaimry <wheal28@gmail.com>
2022-04-22 18:21:58 +0600

    Создана модель Staff унаследованная от AbstractUser & WorkDay поле username создается через поле email

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -1,25 +1,50 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse
 from django.utils.translation import gettext as _
+from phonenumber_field.modelfields import PhoneNumberField
+
+from .managers import CustomUserManager
+from .choice import StaffCategoryChoices, WorkDayChoices
 
 
 class Staff(AbstractUser):
-    CATEGORY_CHOICES = [
-        ('Trainee', 'Стажер'),
-        ('Skilled', 'C опытом'),
-        ('Expert', 'Профессионал'),
-    ]
-
-    inn_passport = models.CharField(max_length=20, unique=True, null=False, blank=False, verbose_name=_('ИНН'))
-    phone = models.CharField(max_length=20, null=False, blank=False, verbose_name=_('Номер телефона'))
+    username = None
+    email = models.EmailField(unique=True, verbose_name=_('Email'))
+    inn_passport = models.CharField(max_length=20, unique=True, null=True, blank=True, verbose_name=_('ИНН'))
+    phone = PhoneNumberField(region="KG", max_length=15, verbose_name=_('Номер телефона'))
     avatar = models.ImageField(upload_to="avatars/", null=True, blank=True, verbose_name=_('Фото профиля'))
-    schedule = models.ManyToManyField('accounts.WorkDay', verbose_name=_('График работы'))
-    experience = models.CharField(max_length=256, choices=CATEGORY_CHOICES, null=False, blank=False,
-                                  default='Trainee', verbose_name=_('Опыт работы'))
-    black_list = models.BooleanField(default=False, verbose_name=_('В черном списке'))
+    address = models.CharField(max_length=200, null=True, blank=True, verbose_name=_('Адрес'))
+    schedule = models.ManyToManyField('accounts.WorkDay', related_name='workday', default='monday',
+                                      verbose_name=_('График работы'))
+    experience = models.CharField(max_length=25, choices=StaffCategoryChoices.choices, default='trainee', null=False,
+                                  blank=False,
+                                  verbose_name=_('Опыт работы'))
+    black_list = models.BooleanField(default=False, verbose_name=_('Черный список'))
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    objects = CustomUserManager()
+
+    def black_list_staff(self):
+        self.black_list = True
+        self.is_active = False
+        self.save()
+
+    def active_staff(self):
+        self.black_list = False
+        self.is_active = True
+        self.save()
+
+    def soft_delete(self):
+        self.is_active = False
+        self.save()
 
     def __str__(self):
         return f'{self.first_name} --- {self.last_name} --- {self.experience} --- {self.phone} '
+
+    def get_absolute_url(self):
+        return reverse('accounts:staff-profile', kwargs={'pk': self.pk})
 
     class Meta:
         db_table = 'Staff'
@@ -28,24 +53,12 @@ class Staff(AbstractUser):
 
 
 class WorkDay(models.Model):
-    DAYS_OF_WEEK = (
-        (1, _('Monday')),
-        (2, _('Tuesday')),
-        (3, _('Wednesday')),
-        (4, _('Thursday')),
-        (5, _('Friday')),
-        (6, _('Saturday')),
-        (0, _('Sunday')),
-    )
-    day_of_the_week = models.IntegerField(
-        verbose_name=_('day of the week'),
-        choices=DAYS_OF_WEEK
-    )
+    day = models.CharField(max_length=25, choices=WorkDayChoices.choices, default='monday', null=False,
+                           blank=False, verbose_name=_('День недели'))
 
     def __str__(self):
-        return f"{self.day_of_the_week}"
+        return self.get_day_display()
 
     class Meta:
-        db_table = 'workdays'
-        verbose_name = _('Рабочий день')
-        verbose_name_plural = _('Рабочие дни')
+        verbose_name = _('День недели')
+        verbose_name_plural = _('Дни недели')
