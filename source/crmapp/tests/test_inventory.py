1b9d8ccd525800eb66159901e486ef42c7f5f652
pythonDev161221 <ormonkenenbaev@gmail.com>
2022-06-21 19:06:21 +0600

    features/#95_1 Дополнение файла test_inventory.py 1

--- source/crmapp/tests/test_inventory.py
+++ source/crmapp/tests/test_inventory.py
@@ -33,25 +33,32 @@ class TestCreateInventory(TestCase):
         self.assertEqual(self.inventory.name, "test_inventory")
         self.assertEqual(self.inventory.description, "test_inventory_description")
 
-        data = {"name": "asfsdf", "description": "dfdsfdsf"}
+    def test_success_create_view_inventory(self):
+        data = {"name": "inventory_35", "description": "description inventory 35"}
         response = self.client.post(reverse("crmapp:inventory_create"), data=data)
         self.assertEqual(response.status_code, 302)
+        self.assertTrue(Inventory.objects.get(name="inventory_35",
+                                              description="description inventory 35"))
 
     def test_success_change_inventory(self):
         self.inventory.name = "test_inventory_2"
         self.inventory.save()
         self.assertEqual(self.inventory.name, "test_inventory_2")
 
-        data = {"name": "asfsdsfdf", "description": "dfdsdfsfdsf"}
+        data = {"name": "inventory_name_23", "description": "inventory description"}
         response = self.client.post(reverse("crmapp:inventory_update", args=[self.inventory.pk]), data=data)
         self.assertEqual(response.status_code, 302)
+        change_inventory = Inventory.objects.get(pk=self.inventory.pk)
+        self.assertEqual(change_inventory.name, "inventory_name_23")
+        self.assertEqual(change_inventory.description, "inventory description")
 
     def test_success_delete_inventory(self):
         inventory = Inventory.objects.create(
             name="test_inventory_3",
             description="test_inventory_description_3"
         )
+        self.assertTrue(Inventory.objects.get(name="test_inventory_3", description="test_inventory_description_3"))
         response = self.client.post(reverse("crmapp:inventory_delete", args=[inventory.pk]))
-        print(response)
         self.assertEqual(response.status_code, 302)
-
+        with self.assertRaises(Inventory.DoesNotExist):
+            Inventory.objects.get(name="test_inventory_3", description="test_inventory_description_3")
