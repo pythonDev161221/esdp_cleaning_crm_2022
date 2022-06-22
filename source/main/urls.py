d3f109213d9c5a340e9ca90b3100762d1f38d968
Mariya <wheal28@gmail.com>
2022-06-23 01:55:23 +0600

    Coздано приложение api, зарегистрировано в setting.py

--- source/main/urls.py
+++ source/main/urls.py
@@ -24,7 +24,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('', include('crmapp.urls')),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
+    path('api/', include('api.urls')),
     # path("telegram-bot/", include("tgbot.urls"))  # Включить при использование бота в режиме webhook
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
