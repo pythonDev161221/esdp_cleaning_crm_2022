200db70743a129b57828f06059a4ed50e230fc7d
Ruslan <xenoncraftkg2@gmail.com>
2022-06-24 15:28:29 +0600

    Features/#101 Добавлены файлы докера и докер композа, также uwsgi

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+cmd="$@"
+until PGPASSWORD=$DB_PASSWORD psql -h "$DB_HOST" -U"$DB_USER" $DB_NAME -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 5
+done
+
+>&2 echo "Postgres is up - executing command"
+
+
+echo "Run python manage.py migrate..." ;
+python source/manage.py migrate ;
+
+exec $cmd
