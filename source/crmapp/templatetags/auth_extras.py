d2e58ca6f171c1a47220f3ec4bb95941e2e54087
denis-sokolov <den.s.glad@gmail.com>
2022-06-24 20:08:50 +0600

    Закончил страницу профиля

--- source/crmapp/templatetags/auth_extras.py
+++ source/crmapp/templatetags/auth_extras.py
@@ -6,3 +6,8 @@ register = template.Library()
 @register.filter(name='has_group')
 def has_group(user, group_name):
     return user.groups.filter(name=group_name).exists()
+
+
+@register.filter(name='cash_order_count')
+def cash_order_count(user):
+    return user.object.manager_cash.filter(is_nullify=False).count()
