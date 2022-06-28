608a540f26ab056f70d3cdfda4068a6df3910222
pythonDev161221 <ormonkenenbaev@gmail.com>
2022-06-29 02:19:16 +0600

    добавление емейла в админку стафа

--- source/accounts/admin.py
+++ source/accounts/admin.py
@@ -4,6 +4,13 @@ from accounts.models import Staff, WorkDay, Payout
 
 User = get_user_model()
 
-admin.site.register(User)
+# admin.site.register(User)
 admin.site.register(WorkDay)
 admin.site.register(Payout)
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'first_name', 'last_name')
+
+
+admin.site.register(User, UserAdmin)
